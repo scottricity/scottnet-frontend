f2cf48560e9f87b66d89c169d31394c1ed8c66d7
Timothy3773 <jonathanscott122@gmail.com>
2022-11-27 18:11:57 -0500

    ye

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import logo from './terminal-solid.svg';
+import * as React from "react";
+import { AppBar , Toolbar , Container, Menu , MenuItem , Typography } from "@mui/material";
+import "@fontsource/ubuntu";
+import "@fontsource/ubuntu-mono";
+import "@fontsource/dotgothic16";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTerminal } from '@fortawesome/free-solid-svg-icons'
+import themeBck from "./theme.js"
+import { ThemeProvider } from '@mui/system';
+function Navi() {
+  return (
+    <ThemeProvider theme={themeBck}>
+      <AppBar position='fixed'>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <FontAwesomeIcon icon={faTerminal} size="2x"/>
+          <Typography fontSize={"30px"} fontFamily={["ubuntu", "monospace"]} sx={{"userSelect": "none"}}>
+            scottnet
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+    <Typography>
+      hi
+    </Typography>
+    </ThemeProvider>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navi></Navi>
   );
 }
 
