5d994a6d86212c6e2278076471379ad161368814
Timothy3773 <jonathanscott122@gmail.com>
2022-12-06 23:00:29 -0500

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,38 @@ import "@fontsource/ubuntu";
 import "@fontsource/ubuntu-mono";
 import "@fontsource/dotgothic16";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTerminal } from '@fortawesome/free-solid-svg-icons'
+import { faTerminal , faWarning , faPersonDigging } from '@fortawesome/free-solid-svg-icons'
 import themeBck from "./theme.js"
 import { ThemeProvider } from '@mui/system';
 import "./style.css"
 
 function Navi() {
-  return (
-    <Container id="navbar" sx={{textAlign: "center", width: "100%"}} maxWidth="auto">
-      <Typography display={'inline'} fontSize="25px">
-        <FontAwesomeIcon icon={faTerminal} />
-        scottnet
-      </Typography>
-    </Container>
-  )
+	return (
+		<Container maxWidth="100%" disableGutters sx={{width: "100%"}}>
+			<Container id="navbar" maxWidth="100%" sx={{ textAlign: "center", width: "100%"}}>
+				<Typography display={'inline'} fontSize="25px">
+					<FontAwesomeIcon icon={faTerminal} />
+					scottnet
+				</Typography>
+			</Container>
+			<Typography sx={{textAlign: "center", marginTop: "10%"}} fontSize="50px">
+			<FontAwesomeIcon icon={faPersonDigging} fontSize="300px" />
+			<br/>
+				This site is under construction!
+			</Typography>
+		</Container>
+	)
 }
 
 function App() {
 
-  return (
-    <ThemeProvider theme={themeBck}>
-      <Navi>
-      </Navi>
+	return (
+		<ThemeProvider theme={themeBck}>
+			<Navi>
+			</Navi>
 
-    </ThemeProvider>
-  );
+		</ThemeProvider>
+	);
 }
 
 export default App;
