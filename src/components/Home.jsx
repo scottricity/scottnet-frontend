07c25b9e0ceb91571c956b77f775ee02d0d56c21
Timothy3773 <jonathanscott122@gmail.com>
2023-02-05 23:23:51 -0500

    more changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,41 @@
-import React, { useState, useEffect } from "react";
-import { AppBar, Toolbar, Container, Menu, MenuItem, Typography, styled, Button, Box, Chip, Divider, Avatar, Card, CardContent, CssBaseline, Grid } from "@mui/material";
+import React, { useState, useEffect, Fragment } from "react";
+import { faTerminal, faWarning, faPersonDigging } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import { AppBar, Toolbar, Container, Menu, MenuItem, Typography, styled, Button, Box, Chip, Divider, Avatar, Card, CardContent, CssBaseline, Grid, Link, Tooltip } from "@mui/material";
+
+var siz = (n) => {
+	return { width: n, height: n }
+}
+class NavBar extends React.Component {
+	render() {
+		return (
+			<Toolbar className="nav" sx={{ gap: "15px", fontWeight: 600 }}>
+				<Avatar src="./selfies/faceshot1.png" sx={siz(50)} />
+				<Typography variant="h5" sx={{ fontFamily: "roboto", fontWeight: "inherit" }}>
+					Jonathan Scott
+				</Typography>
+
+				<Tooltip title="Join my Discord!" arrow>
+					<Link underline="hover" sx={{ marginLeft: `50px`, fontSize: 20 }} href="/discord">
+						Discord
+					</Link>
+				</Tooltip>
+			</Toolbar>
+		)
+	}
+}
 class HomePage extends React.Component {
 	render() {
 		return (
-			<Container>
-				<AppBar sx={{display: "flex"}}>
+			<Box>
+				<NavBar />
+				<Container disableGutters>
 					<Typography>
-						Jonathan Scott
+						It seems this page is underconstruction!
 					</Typography>
-				</AppBar>
-			</Container>
+				</Container>
+			</Box>
 		)
 	}
 }
