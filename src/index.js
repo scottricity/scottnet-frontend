771aee531112bc56d1f5a247447ed91a3a19b976
Timothy3773 <jonathanscott122@gmail.com>
2023-08-13 21:41:47 -0400

    e

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Utils } from "./utils.js";
 
+
 let tester = new Utils()
 console.table(tester.slp([111,222,333,333,334,335, 555, 556]))
 
